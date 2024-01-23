17f408f28ee23b7d40448452d49303041ed7dc9b
JKapturkiewicz <kuba.kapek@gmail.com>
2024-01-23 20:27:49 +0100

    wip: add google envs

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,5 +13,9 @@ const config = () => ({
       },
     },
   },
+  GOOGLE_EMAIL: process.env.GOOGLE_EMAIL,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
 });
 export default config;
