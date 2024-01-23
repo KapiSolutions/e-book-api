161e3761a6afc5e0fb0a86456fa216fe50f1a339
JKapturkiewicz <kuba.kapek@gmail.com>
2024-01-23 18:37:55 +0100

    wip:implement stripe module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StripeModule } from './modules/stripe/stripe.module';
+import config from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    StripeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
