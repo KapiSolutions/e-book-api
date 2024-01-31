71049097d16951d2e136cdf39779c9c5323dc3df
JKapturkiewicz <kuba.kapek@gmail.com>
2024-01-31 18:33:05 +0100

    feat: create CloudStorage provider

---
+++ src/providers/cloudStorage.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage';
+
+@Injectable()
+export class CloudStorage {
+  private readonly storage;
+
+  constructor() {
+    const credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+    if (!credentials) {
+      throw new Error('Credentials does not exist.');
+    }
+
+    this.storage = new Storage({
+      credentials: JSON.parse(credentials),
+    });
+  }
+
+  async downloadFile(bucketName: string, filePath: string): Promise<Buffer> {
+    const bucket = this.storage.bucket(bucketName);
+    const file = bucket.file(filePath);
+
+    const [fileContent] = await file.download();
+
+    return fileContent;
+  }
+}
