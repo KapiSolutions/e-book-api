1d8ad58fe60d4989c2c9e39608b3376b2c2e0001
JKapturkiewicz <kuba.kapek@gmail.com>
2024-03-23 13:05:00 +0100

    feat: implement auth middleware for pdf routes

--- src/modules/pdf/pdf.module.ts
+++ src/modules/pdf/pdf.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PdfService } from './pdf.service';
 import { PdfController } from './pdf.controller';
 import { CloudStorage } from 'src/providers/cloudStorage';
 import { ModifyPDF } from 'src/providers/modifyPDF';
 import { EmailSender } from 'src/providers/emailSender';
+import { AuthMiddleware } from 'src/middleware/auth';
 
 @Module({
   controllers: [PdfController],
   providers: [PdfService, CloudStorage, ModifyPDF, EmailSender],
 })
-export class PdfModule {}
+export class PdfModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('pdf');
+  }
+}
